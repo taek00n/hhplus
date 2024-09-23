274a0ee8c5582d0cadccb816de1e6e3d7b12a328
taek00n <kimth0526@naver.com>
2024-09-23 23:37:13 +0900

    예외처리 및 테스트코드 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,20 +1,25 @@
 package io.hhplus.tdd.point;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class PointService {
 
     private final PointRepository pointRepository;
+    private final Long MAX_POINT = 10000L;
 
     public PointService(PointRepository pointRepository) {
         this.pointRepository = pointRepository;
     }
 
     public UserPoint point(long id) {
-
+        if (id <= 0) {
+            throw new IllegalArgumentException("정수의 아이디만 사용해주세요.");
+        }
         return pointRepository.selectById(id);
     }
 
@@ -25,16 +30,41 @@ public class PointService {
 
     public UserPoint charge(long id, long amount) {
 
-        if (amount < 0) {
-            // 금액은 0 이상
+        changedPoint(id, amount, TransactionType.CHARGE);
+        return pointRepository.selectById(id);
+    }
+
+    public UserPoint use(long id, long amount) {
+
+        changedPoint(id, amount, TransactionType.USE);
+        return pointRepository.selectById(id);
+    }
+
+    // 포인트의 충전과 사용 처리 로직
+    private void changedPoint(long id, long amount, TransactionType type) {
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("금액은 0보다 커야 됩니다.");
         }
 
         UserPoint userPoint = pointRepository.selectById(id);
-        long newPoint = userPoint.point() + amount;
+        long point = userPoint.point();
 
-        pointRepository.insertOrUpdate(id, newPoint);
-        pointRepository.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+        if (type == TransactionType.CHARGE) {
+            point += amount;
+            if (point > MAX_POINT) {
+                throw new IllegalArgumentException("최대 적립 포인트를 초과하였습니다.");
+            }
+            pointRepository.insertOrUpdate(id, point);
+        }
+        else if (type == TransactionType.USE) {
+            point -= amount;
+            if (point < 0) {
+                throw new IllegalArgumentException("포인트가 부족합니다.");
+            }
+            pointRepository.insertOrUpdate(id, point);
+        }
 
-        return pointRepository.selectById(id);
+        pointRepository.insert(id, amount, type, System.currentTimeMillis());
     }
 }
